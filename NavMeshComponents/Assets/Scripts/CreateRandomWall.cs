a2a3205dacb951f1867d97542711cd5db6b79bbc
Matthew Parker <maddparker@gmail.com>
2019-05-06 16:57:51 -0400

    Added NavMesh Project

---
+++ NavMeshComponents/Assets/Scripts/CreateRandomWall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CreateRandomWall : MonoBehaviour
+{
+    public NavMeshSurface surface;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) //if you hit spacebar
+        {
+            GameObject newWall = Instantiate(Resources.Load<GameObject>("Prefabs/Cube"));  //create a cube
+            newWall.transform.position = new Vector3(Random.Range(-7f, 7f), 0.5f, Random.Range(-7f, 7f));//at a random position in the scene
+            surface.BuildNavMesh(); //recalculate the NavMeshSurface based on new walls
+        }
+    }
+}
---
+++ NavMeshComponents/Assets/Scripts/NavMeshController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshController : MonoBehaviour
+{
+    public Vector3 destination = new Vector3(1, 0, 2);
+    Vector3 oldDest; //hold the destination before it changed
+    
+    NavMeshAgent agent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(destination);
+
+        oldDest = destination; //set oldDest to the current destination
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (destination != oldDest) //if destination is no longer = to oldDest
+        {
+            agent.SetDestination(destination); //give the agent a new destination
+            oldDest = destination; //update oldDest to be the current destination
+        }
+
+        if (Input.GetMouseButtonDown(0)) //Was the mouse clicked?
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(ray.origin, ray.direction, Color.green);
+         
+            RaycastHit raycastHit = new RaycastHit();
+
+            if (Physics.Raycast(ray, out raycastHit)) //Raycast returns true if it hits something, and populates raycasthit with info about that hit
+            {
+                destination = raycastHit.point; //set the destination to the location that was clicked on
+            }
+        }
+    }
+}
